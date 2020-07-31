76802485bbcf975ebfc51ddd8a865a3d7a1f5589
luojie <470736911@qq.com>
2020-07-31 13:17:50 +0800

    添加Log

---
+++ src/First/Log.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the Monolog package.
+ *
+ * (c) Jordi Boggiano <j.boggiano@seld.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace First;
+
+
+/**
+ * Monolog log channel
+ *
+ * It contains a stack of Handlers and a stack of Processors,
+ * and uses them to store records that are added to it.
+ *
+ * @author Jordi Boggiano <j.boggiano@seld.be>
+ */
+class Log
+{
+	public function log(){
+		echo 'log....';
+	}
+
+}
